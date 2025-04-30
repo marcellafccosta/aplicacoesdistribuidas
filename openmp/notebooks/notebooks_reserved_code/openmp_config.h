4d01aa7a9a2be1aa3394ad112d6b4970b59fae9d
DanielENeves <danieleugenio.neves@gmail.com>
2025-04-30 18:12:24 -0300

    Fix example on notebook 3. Adding notebook 4

---
+++ openmp/notebooks/notebooks_reserved_code/openmp_config.h
@@ -0,0 +1,4 @@
+#pragma cling add_library_path("/home/daniel/miniconda3/envs/puc_openmp/lib")
+#pragma cling add_include_path("/home/daniel/miniconda3/envs/puc_openmp/include")
+#pragma cling add_include_path("/home/daniel/miniconda3/envs/puc_openmp/lib/gcc/x86_64-conda-linux-gnu/10.4.0/include")
+#pragma cling load("gomp") // Biblioteca de suporte ao OpenMP
