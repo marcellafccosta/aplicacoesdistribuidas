6eb28e9da584eb70b151ecc9451c305336279576
Marcella Chaves <marcellafccosta@gmail.com>
2025-05-28 18:03:49 -0300

    ex 7

---
+++ openmp/notebooks/codigo_externo/exercise_7.cpp
@@ -0,0 +1,60 @@
+#include "openmp_config.h"
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    const int N = 1000;
+    int nthreads, tid, i, j;
+    double **a;
+
+    double start_time, end_time;
+
+    a = (double **)malloc(N * sizeof(double *));
+    for (i = 0; i < N; i++)
+    {
+        a[i] = (double *)malloc(N * sizeof(double));
+    }
+    omp_set_num_threads(8); 
+
+    start_time = omp_get_wtime();
+/* Fork a team of threads with explicit variable scoping */
+#pragma omp parallel shared(nthreads, a) private(i, j, tid)
+    {
+        /* Obtain/print thread info */
+        tid = omp_get_thread_num();
+
+        if (tid == 0)
+        {
+            nthreads = omp_get_num_threads();
+            printf("Number of threads = %d\n", nthreads);
+        }
+        printf("Thread %d starting...\n", tid);
+
+        /* Each thread works on its own private copy of the array */
+        for (i = 0; i < N; i++)
+        {
+            for (j = 0; j < N; j++)
+            {
+                a[i][j] = tid + i + j;
+            }
+        }
+
+        /* For confirmation */
+        printf("Thread %d done. Last element= %f\n", tid, a[N - 1][N - 1]);
+
+    } /* All threads join master thread and disband */
+
+    end_time = omp_get_wtime();
+    printf("Time taken = %f seconds\n", end_time - start_time);
+
+    
+    for (i = 0; i < N; i++)
+    {
+        free(a[i]);
+    }
+    free(a);
+
+    return 0;
+}
